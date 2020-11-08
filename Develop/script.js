cd9e5a99f45a86ea597ea96722e9b1f8ee5b230f
Maria Ortega <mariaortegapb@gmail.com>
2020-11-08 00:23:32 -0500

    added function

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordCriteria = prompt("Would you like uppercase letters?"); 
-var passwordCriteria1 = prompt("Would you like lowercase letters?"); 
-var passwordCriteria2 = prompt("Would you like numbers?"); 
-var passwordCriteria3 = prompt("Would you like special characters (such as ! @ # ?) ]?"); 
+var passwordCriteria = confirm("Would you like uppercase letters?"); 
+var passwordCriteria1 = confirm("Would you like lowercase letters?"); 
+var passwordCriteria2 = confirm("Would you like numbers?"); 
+var passwordCriteria3 = confirm("Would you like special characters (such as ! @ # ?) ]?"); 
 var passwordCriteria4 = prompt("For password lenght, please choose between 8-128 characters.")
 
-var availableChoices = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz",
-, "0123456789", "!@#$%^&*()_+=?"]; 
+//var availableChoices = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz",
+//, "0123456789", "!@#$%^&*()_+=?"]; 
 
-// Write password to the #password input
-function writePassword() {
+
+
+ //Write password to the #password input
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+
+
 //create a function called generate password that will build a password in return
   //start with empty choices array or string and empty password
   //do if conditions to build an array or string of character possibilities
+
+function generate(){
+  var length = document.querySelector("#generate").value;
+  var availableChoices = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=?";
+  var password = ""
+
+  for(var i=0; i<= length; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+
+}
 
 
 //for looping through password criteria 4 lenght and add random character to password
@@ -27,6 +42,16 @@ function writePassword() {
   //var availableChoices = availableChoices[Math.floor(Math.random() * availableChoices.length)]; 
 
   //return the password
+
+
+document.querySelector("#generate").value = password;
+
+}
+
+
+
+
+
 
  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
